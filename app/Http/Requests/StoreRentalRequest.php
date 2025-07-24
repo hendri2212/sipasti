02645d66da3dae9c65b4f3fcaf1fbb63557994c7
hendri2212 <cowok_cool320@yahoo.co.id>
2025-07-24 14:48:25 +0800

    store dan test

---
+++ app/Http/Requests/StoreRentalRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRentalRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'            => ['required','string','max:150'],
+            'phone'           => ['required','string','max:30'],
+            'address'         => ['required','string'],
+            'institution_id'  => ['required','exists:institutions,id'],
+            'asset_id'        => ['required','exists:assets,id'],
+            'photo'           => ['required','image','mimes:jpg,jpeg,png','max:2048'], // 2MB
+        ];
+    }
+}
